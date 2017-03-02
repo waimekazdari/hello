82a7812b9aba9d0a655ea8cc9e0114dc60699334
wiame <kazdari.wiam@gmail.com>
2017-03-02 21:55:45 +0000

    export functions and variables

--- count.js
+++ count.js
@@ -1,4 +1,20 @@
 var counter = function(arr){
-  return 'le tabelau a comme lenght :'+ arr.length ;
+  return 'there are :'+ arr.length +' elements in this array';
 };
-module.exports = counter;
+
+var adder = function(a,b){
+  return a+b
+};
+
+var pi = 3.142;
+/*
+module.exports.counter = counter;
+module.exports.adder = adder;
+module.exports.pi = pi;
+*/
+// we can use also this methode :
+module.exports = {
+counter : counter,
+adder : adder,
+pi : pi
+};
