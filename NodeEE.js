28fb085d2b78293ee3502be6a6913e611c52976e
wiame <kazdari.wiam@gmail.com>
2017-03-02 23:58:04 +0000

    Persone inherite from event

--- NodeEE.js
+++ NodeEE.js
@@ -1,7 +1,29 @@
 var events = require('events');
-var myEvent = new events.EventEmitter();
+/*var myEvent = new events.EventEmitter();
 
 myEvent.on('someEvent',function(msg){
   console.log(msg);
 });
 myEvent.emit('someEvent',"the event was emitted");
+*/
+
+var util = require('util');
+
+var Persone = function(name){
+  this.name = name;
+};
+
+util.inherits(Persone, events.EventEmitter);
+
+var wiame = new Persone('wiame');
+var nora = new Persone('nora');
+var affaf = new Persone('affaf');
+
+var people = [wiame,nora, affaf];
+
+people.forEach(function(persone){
+  persone.on('speak',function(msg){
+    console.log(persone.name + ' said : ' + msg);
+  });
+});
+wiame.emit('speak','hey dudes');
