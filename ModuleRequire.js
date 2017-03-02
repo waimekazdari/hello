f9cfac13b815723e2f19ca24705efa1927239337
wiame <kazdari.wiam@gmail.com>
2017-03-02 21:54:34 +0000

    call count's methods

--- ModuleRequire.js
+++ ModuleRequire.js
@@ -1,2 +1,4 @@
-var counter = require('./count');
-console.log(counter(['wiam','wiama','wiaame']));
+var stuff = require('./count');
+console.log(stuff.counter(['wiam','wiama','wiaame']));
+console.log('the sum of Two nubmers is : ' + stuff.adder(3,6));
+console.log(stuff.adder(6,stuff.pi));
