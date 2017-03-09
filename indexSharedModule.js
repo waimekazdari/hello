fbbee69005d50a2b12c6cce71d7cb30d69429cf1
wiame <kazdari.wiam@gmail.com>
2017-03-09 18:00:28 +0000

    index for sharedModule file

---
+++ indexSharedModule.js
@@ -0,0 +1,6 @@
+var logger = require('./sharedModule');
+var sharedModule = require('./sharedModule');
+
+logger.log('first message to logger');
+sharedModule.log('First message to sharedModule');
+logger.log('Seconde message to Logger');
