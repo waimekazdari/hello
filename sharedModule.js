4e177116f4d86be5edc9d2b6f4cc7d3ef7c67e08
wiame <kazdari.wiam@gmail.com>
2017-03-10 13:10:38 +0000

    updade

--- sharedModule.js
+++ sharedModule.js
@@ -2,5 +2,5 @@ var messageCount = 0;
 
 module.exports.log =function (message) {
   messageCount++;
-  console.log('Message number : '+messageCount +'  is : '+message);
+  console.log('Message number : '+messageCount +'   is : '+message);
 }
